93186541b562bc4dfb7f475cf741147bdf0f4c4d
phillemann <pmidden@gmx.net>
2011-05-14 00:43:37 +0200

    Last fix for fruit::cut_mesh?

--- src/app/fruit/cut_mesh.cpp
+++ src/app/fruit/cut_mesh.cpp
@@ -1,38 +1,30 @@
 #include "cut_mesh.hpp"
 #include "../../math/cut_triangle_at_plane.hpp"
 #include "../../math/triangle_plane_intersection.hpp"
-#include "../../math/generate_texture_coordinates.hpp"
+#include "../../math/line/distance_to_point.hpp"
+#include "../../geometry_traits/vector.hpp"
+#include "../../geometry_traits/box.hpp"
 #include "triangle.hpp"
-#include <sge/renderer/vector3.hpp>
 #include <sge/renderer/matrix4.hpp>
-#include <sge/renderer/vector2.hpp>
-#include <sge/renderer/vector4.hpp>
 #include <sge/renderer/scalar.hpp>
+#include <sge/renderer/vector2.hpp>
+#include <sge/renderer/vector3.hpp>
+#include <sge/renderer/vector4.hpp>
 #include <fcppt/math/vector/vector.hpp>
 #include <fcppt/math/matrix/matrix.hpp>
-#include <fcppt/math/dim/basic_impl.hpp>
-#include <fcppt/math/box/basic_impl.hpp>
+#include <fcppt/math/dim/dim.hpp>
+#include <fcppt/math/box/box.hpp>
+#include <fcppt/math/range_compare.hpp>
 #include <fcppt/assign/make_array.hpp>
-#include <boost/foreach.hpp>
 #include <boost/next_prior.hpp>
 #include <boost/spirit/home/phoenix/bind.hpp>
-#include <boost/spirit/home/phoenix/core/argument.hpp>
-#include <boost/spirit/home/phoenix/operator/arithmetic.hpp>
-#include <boost/spirit/home/phoenix/core/reference.hpp>
-#include <boost/spirit/home/phoenix/operator/self.hpp>
-#include <boost/spirit/home/phoenix/operator/if_else.hpp>
-#include <boost/spirit/home/phoenix/operator/comparison.hpp>
-#include <boost/range/algorithm_ext/iota.hpp>
+#include <boost/spirit/home/phoenix/core.hpp>
 #include <boost/range/algorithm/find_if.hpp>
-#include <boost/range/algorithm/min_element.hpp>
 #include <boost/range/algorithm/copy.hpp>
-
+#include <boost/mpl/identity.hpp>
 #include <boost/geometry/geometry.hpp>
 #include <boost/geometry/multi/multi.hpp>
 #include <boost/geometry/multi/algorithms/detail/for_each_range.hpp>
-#include "../../geometry_traits/vector.hpp"
-#include "../../geometry_traits/box.hpp"
-
 #include <algorithm>
 #include <vector>
 #include <set>
@@ -135,22 +127,15 @@ triangle_area(
 }
 
 template<typename Container>
-struct make_coordinate_system_msvc_hack
-{
-	typedef typename
-	Container::value_type::value_type
-	scalar_type;
-
-	typedef typename
-	fcppt::math::matrix::static_<scalar_type,4,4>::type
-	type;
-};
-
-template<typename Container>
 fcppt::optional
 <
-	typename
-	make_coordinate_system_msvc_hack<Container>::type
+	typename 
+	fcppt::math::matrix::static_
+	<
+		typename boost::mpl::identity<typename Container::value_type>::type::value_type,
+		4,
+		4
+	>::type
 > const
 make_coordinate_system(
 	Container const &points,
@@ -334,8 +319,8 @@ fruitcut::app::fruit::cut_mesh(
 			++current_is_point)
 			border.push_back(
 				*current_is_point);
-		/*
 		// Only add new points if they're not already in the set
+		/*
 		BOOST_FOREACH(
 			intersection::point_sequence::const_reference r,
 			single_result.points())
@@ -622,186 +607,10 @@ fruitcut::app::fruit::cut_mesh(
 							current_vertex-1]))
 					(transform_texcoord(
 						texcoords[
-							current_vertex-2]))));
+							current_vertex]))));
 
 		output_area += 
 			triangle_area(
 				output_mesh.triangles.back());
 	}
-
-
-	
-
-
-
-
-
-
-
-#if 0
-
-
-
-
-
-	typedef
-	std::vector<sge::renderer::vector2>
-	texcoord_vector;
-
-	// No const here, we want to delete some of the points
-	texcoord_vector texcoords = 
-		border.empty()
-		?
-			texcoord_vector()
-		:
-			math::generate_texture_coordinates<texcoord_vector>(
-				border);
-
-	/*
-	std::cout << "------------------- Texcoords begin \n";
-	BOOST_FOREACH(
-		texcoord_vector::const_reference r,
-		texcoords)
-		std::cout << "{" << r[0] << "," << r[1] << "},\n";
-	std::cout << "------------------- Texcoords end \n";
-	*/
-
-	typedef
-	std::vector<texcoord_vector::size_type>
-	index_sequence;
-
-	// We cannot sort a container 'A' with a criterion which is stored
-	// in container 'B', so we have to detour over this index_sequence
-	index_sequence indices(
-		border.size());
-
-	boost::iota(
-		indices,
-		static_cast<index_sequence::value_type>(0));
-
-	// Step 1: Find the point with the lowest y coordinate (note the
-	// sloppy equality and the lexicographical comparison)
-	texcoord_vector::iterator min_y_it = 
-		boost::min_element(
-			texcoords,
-			boost::phoenix::if_else(
-				boost::phoenix::bind(
-					&nearly_equals<sge::renderer::scalar>,
-					boost::phoenix::arg_names::arg1[1],
-					boost::phoenix::arg_names::arg2[1],
-					epsilon),
-				boost::phoenix::arg_names::arg1[0] < 
-					boost::phoenix::arg_names::arg2[0],
-				boost::phoenix::arg_names::arg1[1] < 
-					boost::phoenix::arg_names::arg2[1]));
-
-	//std::cout << "Minimalpunkt: " << "{" << (*min_y_it)[0] << "," << (*min_y_it)[1] << "},\n";
-
-	// WLOG, the first element is the one with the lowest y coordinates
-	std::swap(
-		*min_y_it,
-		*texcoords.begin());
-	std::swap(
-		*(border.begin() + std::distance(texcoords.begin(),min_y_it)),
-		*border.begin());
-
-	sge::renderer::vector2 const min_y = 
-		*texcoords.begin();
-
-	// This is a set because maybe we delete a point more than once
-	typedef
-	std::set
-	<
-		index_sequence::value_type,
-		std::greater<index_sequence::value_type>
-	>
-	index_set;
-
-	// I think this is technically an optimization: If "min_y" and two
-	// other points are colinear, delete the nearest point after min_y.
-	// TODO: Can we make this more pretty?
-	index_set to_delete;
-	for (index_sequence::value_type i = 1; i < border.size(); ++i)
-		for (index_sequence::value_type j = i+1; j < border.size(); ++j)
-			if(
-				std::abs(
-					compare_angles(
-						min_y,
-						texcoords[i],
-						texcoords[j])) < epsilon)
-				to_delete.insert(
-					fcppt::math::vector::length(min_y - texcoords[i]) 
-						< fcppt::math::vector::length(min_y - texcoords[j]) 
-					? 
-						i
-					:
-						j);
-
-//	std::cout << "Deleted texcoords begin\n";
-	BOOST_FOREACH(
-		index_set::const_reference r,
-		to_delete)
-	{
-	//	std::cout << "{" << (*(texcoords.begin() + static_cast<texcoord_vector::difference_type>(r)))[0] << "," << (*(texcoords.begin() + static_cast<texcoord_vector::difference_type>(r)))[1] << "},\n";
-		texcoords.erase(
-			texcoords.begin() + static_cast<texcoord_vector::difference_type>(r));
-		border.erase(
-			border.begin() + static_cast<texcoord_vector::difference_type>(r));
-	}
-//	std::cout << "Deleted texcoords end\n";
-
-	// Now we have to create a new index array!
-	indices.resize(
-		border.size());
-	boost::iota(
-		indices,
-		static_cast<index_sequence::value_type>(0));
-
-	// Finally, sort the indices by the texcoord angles
-	std::sort(
-		boost::next(
-			indices.begin()),
-		indices.end(),
-		boost::phoenix::bind(
-			&compare_angles,
-			min_y,
-			boost::phoenix::ref(texcoords)[boost::phoenix::arg_names::arg1],
-			boost::phoenix::ref(texcoords)[boost::phoenix::arg_names::arg2]) 
-			< static_cast<sge::renderer::scalar>(0));
-
-//	std::cout << "Sorted points begin\n";
-	/*
-	BOOST_FOREACH(
-		index_sequence::const_reference r,
-		indices)
-	*/
-	//	std::cout << "{" << border[r][0] << "," << border[r][1] << "," << border[r][2] << "},\n";
-//	std::cout << "Sorted points end\n";
-
-	for(
-		index_sequence::const_iterator i = boost::next(indices.begin()); 
-		i != boost::prior(indices.end()); 
-		++i)
-	{
-		result_mesh.triangles.push_back(
-			triangle(
-				fcppt::assign::make_array<triangle::vector>
-					(border[indices.front()] - barycenter)
-					(border[*i] - barycenter)
-					(border[*boost::next(i)] - barycenter),
-				fcppt::assign::make_array<triangle::data_type>
-					(transform_texcoord(
-						texcoords[indices.front()]))
-					(transform_texcoord(
-						texcoords[*i]))
-					(transform_texcoord(
-						texcoords[
-							*boost::next(
-								i)]))));
-			
-		area += 
-			triangle_area(
-				result_mesh.triangles.back());
-	}
-#endif
 }
