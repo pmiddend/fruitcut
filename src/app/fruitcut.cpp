281b5eb6d88cde7810d9f5de57571ec8ec709105
phillemann <pmidden@gmx.net>
2011-06-19 20:27:03 +0200

    Added SIGSEGV handler

--- src/app/fruitcut.cpp
+++ src/app/fruitcut.cpp
@@ -3,13 +3,84 @@
 #include <fcppt/scoped_state_machine.hpp>
 #include <fcppt/exception.hpp>
 #include <fcppt/io/cerr.hpp>
+#include <fcppt/config.hpp>
 #include <iostream>
+
+#ifndef FCPPT_HAVE_POSIX
+#include <fcppt/noncopyable.hpp>
+#include <fcppt/backtrace/print_current_stack_frame.hpp>
+#include <signal.h>
+#include <cstdlib>
+
+namespace
+{
+class signal_stack_printer
+{
+FCPPT_NONCOPYABLE(
+	signal_stack_printer);
+public:
+	explicit
+	signal_stack_printer(
+		int const _signal_type)
+	:
+		signal_type_(
+			_signal_type)
+	{
+		sigaction_.sa_sigaction = 
+			&callback;
+		// sa_mask specifies which signals should be blocked during the
+		// handler execution
+		sigemptyset(
+			&sigaction_.sa_mask);
+		// SA_SIGINFO: Ignore sa_handler (which lets you set "ignore" and
+		// "use default handler") and use sa_sigaction as callback.
+		// SA_RESTART: I don't get what it does
+		sigaction_.sa_flags = SA_RESTART | SA_SIGINFO;
+
+		sigaction(
+			signal_type_,
+			&sigaction_,
+			// Old action pointer
+			&old_sigaction_);
+	}
+
+	~signal_stack_printer()
+	{
+		sigaction(
+			signal_type_,
+			&old_sigaction_,
+			// Old action pointer
+			0);
+	}
+private:
+	int const signal_type_;
+	struct sigaction sigaction_;
+	struct sigaction old_sigaction_;
+
+	static void 
+	callback(
+		int /*signal*/,
+		siginfo_t */*info*/,
+		void *)
+	{
+		std::cerr << "Got a SIGSEGV, printing backtrace:\n";
+		fcppt::backtrace::print_current_stack_frame();
+		std::exit(-1);
+	}
+};
+}
+#endif
 
 int main(
 	int argc,
 	char *argv[])
 try
 {
+#ifndef FCPPT_HAVE_POSIX
+	signal_stack_printer stack_printer(
+		SIGSEGV);
+#endif
+
 	fruitcut::app::machine m(
 		argc,
 		argv);
