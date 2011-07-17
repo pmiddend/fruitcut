68139a047eb69256d5978e275595e80644479fbf
phillemann <pmidden@gmx.net>
2011-07-17 22:15:08 +0200

    The server now generates the date time string. Moved unproject to fruitlib

---
+++ src/server/generate_datetime.hpp
@@ -0,0 +1,15 @@
+#ifndef FRUITCUT_SERVER_GENERATE_DATETIME_HPP_INCLUDED
+#define FRUITCUT_SERVER_GENERATE_DATETIME_HPP_INCLUDED
+
+#include <string>
+
+namespace fruitcut
+{
+namespace server
+{
+std::string const
+generate_datetime();
+}
+}
+
+#endif
