806d11d6cbfe6bd2df9cea41a2c47fb147f52eeb
Philipp Reh <sefi@s-e-f-i.de>
2012-10-15 20:47:32 +0200

    Fixed two headers.

--- include/fruitapp/ambient_intensity.hpp
+++ include/fruitapp/ambient_intensity.hpp
@@ -1,6 +1,7 @@
 #ifndef FRUITAPP_AMBIENT_INTENSITY_HPP_INCLUDED
 #define FRUITAPP_AMBIENT_INTENSITY_HPP_INCLUDED
 
+#include <sge/renderer/scalar.hpp>
 #include <fcppt/strong_typedef.hpp>
 
 namespace fruitapp
